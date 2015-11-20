2093f3c99b18cf4385b38c2db2b8c2f8fe8a1d82
Andy Macnamara <macna@users.noreply.github.com>
2015-11-20 16:47:15 +0000

    Initial commit.
    Initial commit of project resources.

---
+++ adfs-powershell-management/update-msol-domains.ps1
@@ -0,0 +1,10 @@
+﻿# Import the MSOnline PowerShell module
+Import-Module MSOnline
+
+# Define credentials and connect to the tenant
+$cred = Get-Credential -Message "Enter credentials for a cloud-only (i.e. 'username@tenant.onmicrosoft.com') account that has the 'Global Administrator' role."
+Connect-MsolService -Credential $cred
+
+# Get the list of domains that are currently federated with Microsoft Online Services and store them in $fedDomains
+$fedDomains = Get-MsolDomain | Select-Object Name
+
