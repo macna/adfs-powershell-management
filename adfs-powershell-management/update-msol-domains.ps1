e858b615a4877fbdaa7a0a7b26e6b4dc73678e9e
Andy Macnamara <macna@users.noreply.github.com>
2015-11-25 11:51:04 +0000

    Added IF statement
    Added an IF statement so that only federated domains are worked with.

--- adfs-powershell-management/update-msol-domains.ps1
+++ adfs-powershell-management/update-msol-domains.ps1
@@ -1,10 +1,29 @@
-﻿# Import the MSOnline PowerShell module
+﻿#If the new token-decrypting and token-signing certificates haven't flipped from secondary to primary yet, do it
+$certPopup = New-Object -ComObject Wscript.Shell
+$certPopup.Popup("Before proceeding, make sure that you have switched the token-decrypting and token-signing certificates on the primary AD FS server.")
+
+# Import the MSOnline PowerShell module
 Import-Module MSOnline
 
 # Define credentials and connect to the tenant
 $cred = Get-Credential -Message "Enter credentials for a cloud-only (i.e. 'username@tenant.onmicrosoft.com') account that has the 'Global Administrator' role."
 Connect-MsolService -Credential $cred
 
-# Get the list of domains that are currently federated with Microsoft Online Services and store them in $fedDomains
-$fedDomains = Get-MsolDomain | Select-Object Name
+# Get the list of domains that are currently federated with Microsoft Online Services and store them in $domainList
+$domainList = Get-MsolDomain
 
+ForEach ($domain In $domainList)
+	{
+		$domain
+		$domainName = $domain.Name
+		$domainAuth = $domain.Authentication
+
+		If ($domainAuth -eq "Federated")
+			{
+				Update-MsolFederatedDomain -DomainName $domainName -SupportMultipleDomain
+			}
+	}
+
+# Let the user know when completed
+$endPopup = New-Object -ComObject Wscript.Shell
+$endPopup.Popup("The update process has completed.")
